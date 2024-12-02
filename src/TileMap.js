75d4e9dce7eb06ae62164b9cc90449e00c418ff0
GitbotAVX <botaero092@gmail.com>
2024-12-01 17:12:43 -0800

    finalize game

--- src/TileMap.js
+++ src/TileMap.js
@@ -221,4 +221,12 @@ export default class TileMap {
     }
     return false;
   }
+
+  didWin() {
+    return this.#dotsLeft() === 0;
+  }
+
+  #dotsLeft() {
+    return this.map.flat().filter((tile) => tile === 0).length;
+  }
 }
